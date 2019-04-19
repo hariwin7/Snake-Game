1fb90bb0aa2744a58ff9dd02474c337b067ce8ee
hariprasad kb <hariwin7@gmai.com>
2019-04-19 20:35:16 +0530

    Updates

---
+++ precache-manifest.d79d24d9192fd5e7f06417593cefb9fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2bbf63ab1f5cf281319a",
+    "url": "/Snake-Game/static/js/runtime~main.a203c289.js"
+  },
+  {
+    "revision": "74a67edfe66124dc4731",
+    "url": "/Snake-Game/static/js/main.b699e996.chunk.js"
+  },
+  {
+    "revision": "5eb3fccec92c8bd1838f",
+    "url": "/Snake-Game/static/js/2.9a6710f9.chunk.js"
+  },
+  {
+    "revision": "74a67edfe66124dc4731",
+    "url": "/Snake-Game/static/css/main.83e4564a.chunk.css"
+  },
+  {
+    "revision": "5eb3fccec92c8bd1838f",
+    "url": "/Snake-Game/static/css/2.3d3d88d8.chunk.css"
+  },
+  {
+    "revision": "83566172b567d369e27956ddc4f8841e",
+    "url": "/Snake-Game/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dcf7a26e492179983da8183aac0f7f04.js"
+  "/Snake-Game/precache-manifest.d79d24d9192fd5e7f06417593cefb9fd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Snake-Game/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
