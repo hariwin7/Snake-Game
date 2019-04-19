1134edc2cdad0cfb660694e246d053253f5c87a3
hariprasad kb <hariwin7@gmai.com>
2019-04-19 20:26:11 +0530

    Updates

---
+++ precache-manifest.dcf7a26e492179983da8183aac0f7f04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "06140e06ca23349c1888",
+    "url": "/static/js/main.6c9971dc.chunk.js"
+  },
+  {
+    "revision": "5eb3fccec92c8bd1838f",
+    "url": "/static/js/2.9a6710f9.chunk.js"
+  },
+  {
+    "revision": "06140e06ca23349c1888",
+    "url": "/static/css/main.83e4564a.chunk.css"
+  },
+  {
+    "revision": "5eb3fccec92c8bd1838f",
+    "url": "/static/css/2.3d3d88d8.chunk.css"
+  },
+  {
+    "revision": "b343f42f3c1391f9ae88d211fd665b70",
+    "url": "/index.html"
+  }
+];
